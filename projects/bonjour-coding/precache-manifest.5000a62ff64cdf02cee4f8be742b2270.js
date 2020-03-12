d8badbc9f32f40a41dede3cd3a592efefc068b7d
ravjs <konanravier@gmail.com>
2020-03-12 13:54:24 -0400

    Added project bonjour-coding

---
+++ projects/bonjour-coding/precache-manifest.5000a62ff64cdf02cee4f8be742b2270.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41aa45af241da94e51305089ee48db9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2acb49ae595cf6fa009f",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "9456106319590850e2df",
+    "url": "/static/css/main.90d2770e.chunk.css"
+  },
+  {
+    "revision": "2acb49ae595cf6fa009f",
+    "url": "/static/js/2.43ee0f90.chunk.js"
+  },
+  {
+    "revision": "9456106319590850e2df",
+    "url": "/static/js/main.c14420a3.chunk.js"
+  },
+  {
+    "revision": "c519b7bb6365a01eede0",
+    "url": "/static/js/runtime-main.5a2a449b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
